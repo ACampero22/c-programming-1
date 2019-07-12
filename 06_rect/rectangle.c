90ed50f67f5ba4f5caca097e72f745b20a2ac539
Coursera Learner <nobody@nowhere.nul>
2019-07-12 02:00:17 +0000

    assigment done

--- 06_rect/rectangle.c
+++ 06_rect/rectangle.c
@@ -16,14 +16,56 @@ int max (int a, int b) {
 }
 
 //Declare your rectangle structure here!
-
+typedef struct rect{
+  int x;
+  int y;
+  int width;
+  int height;
+} rectangle;
 
 rectangle canonicalize(rectangle r) {
   //WRITE THIS FUNCTION
+  if(r.width<0){
+    r.x += r.width;
+    r.width *= -1;
+  }
+  if(r.height<0){
+    r.y += r.height;
+    r.height *= -1;
+  }
   return r;
 }
+
+
 rectangle intersection(rectangle r1, rectangle r2) {
   //WRITE THIS FUNCTION
+  r1 = canonicalize(r1);
+  r2 = canonicalize(r2);
+
+  //CHECK OVERLAPPING CONDITION
+  int tmpx = min(r1.x, r2.x);
+  int tmpy = min(r1.y, r2.y);
+  if (((tmpx == r1.x)&&(r2.x > r1.x + r1.width))||((tmpy==r1.y)&&(r2.y > r1.y + r1.height))){
+    r1.width=0;
+    r1.height=0;
+    return r1;
+  }
+  if(((tmpx == r2.x)&&(r1.x > r2.x + r2.width))||((tmpy==r2.y)&&(r1.y > r2.y + r2.height))){
+    r1.width=0;
+    r1.height=0;
+    return r1;
+  }
+  
+  int x = max(r1.x, r2.x);
+  int y = max(r1.y, r2.y);
+  int width = min(r1.x+r1.width, r2.x+r2.width) - x;
+  int height = min(r1.y+r1.height, r2.y+r2.height) - y;
+
+  r1.x = x;
+  r1.y = y;
+  r1.width = width;
+  r1.height = height;
+  
   return r1;
 }
 
