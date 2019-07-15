7b0500ef26871923a95a5b9296c10cb7840c62d7
Coursera Learner <nobody@nowhere.nul>
2019-07-15 16:41:02 +0000

    done

---
+++ 15_tests_subseq/test-subseq.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t maxSeq(int * array, size_t n);
+
+int main (void){
+
+  int arr[]={};
+  size_t ans=maxSeq(arr,0);
+  if (ans != 0) return EXIT_FAILURE;
+
+  int arr1[]={ 1, 2, 1, 3, 5, 7, 2, 4, 6, 9};
+  ans=maxSeq(arr1,10);
+  if (ans != 4 ) return EXIT_FAILURE;
+
+  int arr2[]={1,2,3,4};
+  ans=maxSeq(arr2,4);
+  if (ans != 4) return EXIT_FAILURE;
+
+  int arr5[]={1,3,3,5};
+  ans=maxSeq(arr5,4);
+  if (ans != 2 ) return EXIT_FAILURE;
+
+  int ar[]={5,-6,7,8};
+  ans=maxSeq(ar,4);
+  if (ans != 3) return EXIT_FAILURE;
+
+  int ar1[]={10,9,7,6};
+  ans=maxSeq(ar1,4);
+  if (ans != 1) return EXIT_FAILURE;
+
+  return EXIT_SUCCESS;
+
+
+}
