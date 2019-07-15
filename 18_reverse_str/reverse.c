1c87013ce7fafaee6cda3d47d1c8268daec7f1ed
Coursera Learner <nobody@nowhere.nul>
2019-07-15 20:07:32 +0000

    done

--- 18_reverse_str/reverse.c
+++ 18_reverse_str/reverse.c
@@ -3,7 +3,15 @@
 #include <string.h>
 
 void reverse(char * str) {
-  //WRITE ME!
+  if(str==NULL) return;
+  char tmp;
+  size_t len = strlen(str);
+  for(size_t i=0; i<len/2; i++){
+    tmp = str[len-i-1];
+    str[len-i-1] = str[i];
+    str[i] = tmp;
+  }
+  return;
 }
 
 int main(void) {
