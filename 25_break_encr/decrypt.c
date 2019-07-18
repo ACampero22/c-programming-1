e607d6a03c8ef44415e72c0ebe361d117cc97d64
Coursera Learner <nobody@nowhere.nul>
2019-07-18 05:04:03 +0000

    first attemp

---
+++ 25_break_encr/decrypt.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int decrypt(FILE *f){
+  int c, arr[26]={0};
+
+  //get char freq count
+  while((c=fgetc(f))!=EOF){
+    if(isalpha(c)){
+      c = tolower(c);
+      c -= 'a';
+      arr[c]++;
+    }
+  }
+
+  //get highest frequency char
+  int hfc, cnt=0;
+  for(int i=0; i<26; i++){
+    if(arr[i]>cnt){
+      cnt = arr[i];
+      hfc = i;
+    }
+  }
+
+  //get decryption
+  if(hfc-'e'>=0) return  hfc - 'e';
+  else return 26 + hfc - 'e';
+
+}
+
+int main(int argc, char ** argv){
+
+  //check cml args
+  if(argc != 2){
+    fprintf(stderr, "Please just provide an input file for decrption.\n");
+    return EXIT_FAILURE;
+  }
+
+  FILE * f = fopen(argv[1], "r");
+
+  if(f==NULL){
+    perror("invalid input file");
+    return EXIT_FAILURE;
+  }
+
+  int ans = decrypt(f);
+  printf("%d", ans);
+
+  //close file
+  if(fclose(f)!=0){
+    perror("Fail to close the input file");
+    return EXIT_FAILURE;
+  }
+  
+  return EXIT_SUCCESS;
+}
