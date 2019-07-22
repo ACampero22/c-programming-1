8d5692572980a462993d6d2ed073e357ba28e76c
Coursera Learner <nobody@nowhere.nul>
2019-07-22 02:08:08 +0000

    done

--- c3prj2_eval/eval.c
+++ c3prj2_eval/eval.c
@@ -180,11 +180,22 @@ int compare_hands(deck_t * hand1, deck_t * hand2) {
 //use of get_match_counts.
 unsigned * get_match_counts(deck_t * hand){
   unsigned * arr = malloc((hand->n_cards)*sizeof(*arr));
+  /*
+  for(int i=0; i<hand->n_cards; i++){
+    card_t x = *(hand->cards[i]);
+    unsigned count = 0;
+    for(int j=0; j<hand->n_cards;j++){
+      if(x.value == (hand->cards[j])->value) count++;
+    }
+    arr[i] = count;
+    }*/
+
+  
   unsigned count = 1;
   int i = 1;
 
   while(i<hand->n_cards){
-    if(hand->cards[i]->value==hand->cards[i-1]->value){
+    if((hand->cards[i])->value==(hand->cards[i-1])->value){
       count++;
       if(i==hand->n_cards-1){
 	for(int j=i-count+1; j<i+1; j++){
